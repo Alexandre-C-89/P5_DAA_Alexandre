c7d39e9f5b5b819f0dd46f262ea262ac4bedbe7b
Clémençot Alexandre <73541197+Alexandre-C-89@users.noreply.github.com>
2023-09-25 17:12:11 +0200

    Correction doublon de code + tests

--- app/src/androidTest/java/com/cleanup/todoc/MainActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/cleanup/todoc/MainActivityInstrumentedTest.java
@@ -4,12 +4,6 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.assertTrue;
 import androidx.test.runner.AndroidJUnit4;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @author Gaëtan HERFRAY
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityInstrumentedTest {
 
--- app/src/androidTest/java/com/cleanup/todoc/TaskDaoTest.java
+++ app/src/androidTest/java/com/cleanup/todoc/TaskDaoTest.java
@@ -1,6 +1,5 @@
 package com.cleanup.todoc;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
